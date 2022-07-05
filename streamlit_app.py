c0ad481266747e8b1ce6da08284b81745b05fc11
snowflaketest43 <108729900+snowflaketest43@users.noreply.github.com>
2022-07-05 11:18:41 +0100

    Update streamlit_app.py
    new text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My title')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Oemga 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
